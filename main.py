bb69a0d2da6658aa3484629f17f7af44544da341
gyubin <goubin55@naver.com>
2023-04-03 17:44:15 +0900

    커밋4
    복원

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 ##메서드 선언부
 def add_func(n1, n2):
-    res = n1+n2
-    return res
+    return n1+n2
+    
 
 def sub_func(n1,n2):
     return n1-n2
+
+def mul_func(n1,n2):
+    return n1*n2
+
+def div_func(n1,n2):
+    return n1/n2
 
 ##전역 변수부(클래스 변수, 인스턴스 변수)
 num1, num2, result= 100, 200, 0
@@ -16,3 +22,9 @@ print(num1, '+', num2, '=', result)
 
 result =sub_func(num1, num2)
 print(num1,'-',num2,'=',result)
+
+result=mul_func(num1, num2)
+print(num1,'x',num2,'=',result)
+
+result=div_func(num1, num2)
+print(num1,'/',num2,'=',result)
