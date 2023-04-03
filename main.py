75e0e643dd2a176cf3e3b1c916c76fea946656ed
gyubin <goubin55@naver.com>
2023-04-03 17:48:03 +0900

    MyCommit3
    제곱함수 추가

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def mul_func(n1,n2):
 def div_func(n1,n2):
     return n1/n2
 
+def squ_func(n1):
+    return n1*n1
+
 ##전역 변수부(클래스 변수, 인스턴스 변수)
 num1, num2, result= 100, 200, 0
 
@@ -28,3 +31,6 @@ print(num1,'x',num2,'=',result)
 
 result=div_func(num1, num2)
 print(num1,'/',num2,'=',result)
+
+result=squ_func(num1)
+print(num1,'^2 =',result)
